9b601b5a34691639af59cf054d36914062d9b690
shiva3110 <shivaskd.v107@gmail.com>
2026-06-20 18:21:51 +0530

    folder-push

---
+++ src/main/java/com/tao/apis/hello_world/controller/ApiController.java
@@ -0,0 +1,17 @@
+package com.tao.apis.hello_world.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello..";
+    }
+}
+
+//https://www.meesho.com/api/v1/products/
